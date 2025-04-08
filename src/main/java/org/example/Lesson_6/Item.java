ce21ee28ce62f69f33f412b84173988e5504dce2
AlekseyPuzenko <cd347@yandex.ru>
2025-04-08 18:34:24 +0300

    Lesson_6_fixed

---
+++ src/main/java/org/example/Lesson_6/Item.java
@@ -0,0 +1,24 @@
+package org.example.Lesson_6;
+
+public class Item {
+    private String name;
+    private String created;
+    private String creator;
+    private String country;
+    private double price;
+    private Boolean reserved;
+
+    public Item(String name, String created, String creator, String country, double price, Boolean reserved) {
+        this.name = name;
+        this.created = created;
+        this.creator = creator;
+        this.country = country;
+        this.price = price;
+        this.reserved = reserved;
+    }
+
+    public void displayInfo() {
+        System.out.printf("Name: %s \ncreated: %s \ncreator: %s \ncountry: %s \nprice: %.2f \nreserved: %b\n",
+                name, created, creator, country, price, reserved);
+    }
+}
