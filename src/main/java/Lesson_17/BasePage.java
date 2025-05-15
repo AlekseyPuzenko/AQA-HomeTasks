575275d361aef4061cb2beeaadac281d21ac6b37
AlekseyPuzenko <cd347@yandex.ru>
2025-05-15 21:22:48 +0300

    Lesson_17

---
+++ src/main/java/Lesson_17/BasePage.java
@@ -0,0 +1,20 @@
+package Lesson_17;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class BasePage {
+    protected static final String BASE_URL = "https://postman-echo.com";
+
+    protected RequestSpecification request;
+
+    public BasePage() {
+        RestAssured.baseURI = BASE_URL;
+        request = RestAssured.given();
+    }
+
+    protected Response sendRequestAndGetResponse() {
+        return request.when().get();
+    }
+}
---
+++ src/main/java/Lesson_17/GetPage.java
@@ -0,0 +1,22 @@
+package Lesson_17;
+
+import io.restassured.response.Response;
+
+public class GetPage extends BasePage {
+    private static final String GET_ENDPOINT = "/get";
+
+    public GetPage() {
+        super();
+        request.basePath(GET_ENDPOINT);
+    }
+
+    public GetPage withQueryParam(String paramName, String paramValue) {
+        request.queryParam(paramName, paramValue);
+        return this;
+    }
+
+    public Response sendGetRequest() {
+        return sendRequestAndGetResponse();
+    }
+
+}
